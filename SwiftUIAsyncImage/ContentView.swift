2dec915d253814dfb723c7b0a9ab82788a8f1b56
Renato Ribeiro <renato.ribeiro@ucode.pt>
2022-02-22 14:56:15 +0000

    Initial Commit

---
+++ SwiftUIAsyncImage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Renato Ribeiro on 22/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Renato Ribeiro on 22/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
