4622b1b9782efde75ae209423d2bb4835cd32de2
Renato Ribeiro <renato.ribeiro@ucode.pt>
2022-02-22 15:57:33 +0000

    Finish this application Async with Swift

--- SwiftUIAsyncImage/ContentView.swift
+++ SwiftUIAsyncImage/ContentView.swift
@@ -7,10 +7,80 @@
 
 import SwiftUI
 
+extension Image{
+    func imageModifier() -> some View{
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func iconModifier() -> some View{
+        self
+            .imageModifier()
+            .frame(maxWidth: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
 struct ContentView: View {
+    private let imageUrl: String = "https://credo.academy/credo-academy@3x.png"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // MARK: - 1. Basic
+        //AsyncImage(url: URL(string: imageUrl))
+        
+        // MARK: - 2. SCALE
+        //AsyncImage(url: URL(string: imageUrl), scale: 3.0)
+        
+        // MARK: - 3. PLACEHOLDER
+        /*
+        AsyncImage(url: URL(string: imageUrl)){ image in
+            image.imageModifier()
+                
+        } placeholder: {
+            Image(systemName: "photo.circle.fill")
+                .iconModifier()
+        }
+        .padding(40)
+         */
+        
+        // MARK: - 4. PHASE
+        /*
+        AsyncImage(url: URL(string: imageUrl)){ phase in
+            // SUCCESS: The image successfully loaded.
+            // FAILURE: The image failed to load with an error.
+            // EMPTY: No image is loaded
+            
+            if let image = phase.image {
+                image.imageModifier()
+            }else if phase.error != nil{
+                Image(systemName: "ant.circle.fill").iconModifier()
+            }else{
+                Image(systemName: "photo.circle.fill").iconModifier()
+            }
+        }
+        .padding(40)
+         */
+        
+        // MARK: - 5. ANIMATION
+        AsyncImage(url: URL(string: imageUrl), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
+            switch phase{
+            case .success(let image):
+                image
+                    .imageModifier()
+                    //.transition(.move(edge: .bottom))
+                    //.transition(.slide)
+                    .transition(.scale)
+            case .failure(_):
+                Image(systemName: "ant.circle.fill").iconModifier()
+            case .empty:
+                Image(systemName: "photo.circle.fill").iconModifier()
+            @unknown default:
+                ProgressView()
+            }
+        }
+        .padding(40)
     }
 }
 
